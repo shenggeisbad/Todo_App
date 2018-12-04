a554e73bfd58c2a2a80befc1ff1a8fa9406b2340
Sheng Ge <shengge69@hotmail.com>
2018-12-04 22:11:33 +0800

    Updates styles of homepage

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Avatar } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Avatar
+  xlarge
+  rounded
+  source={{uri: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12213209/West-Highland-White-Terrier-on-White-01.jpg"}}
+  onPress={() => console.log("Koloqyok")}
+  activeOpacity={0.7}
+/>
+        <Text> Pat Me!</Text>
+        <Button title={"HEHE"} onPress={() => { console.log("holoq holoq"); }}/>
       </View>
     );
   }
@@ -14,7 +23,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
